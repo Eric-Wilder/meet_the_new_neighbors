2e8a7582a8c5cf8a8422a71dd75ede715bb49d60
Ed Sivick <ed@sivick.net>
2021-01-15 17:41:20 -0500

    remove ability to delete entry, update code & notes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,19 +1,15 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
+  // Gets all families
   getFamilies: function() {
     return axios.get("/api/families");
   },
-  // Gets the book with the given id
+  // Gets the family with the given id
   getFamily: function(id) {
     return axios.get("/api/families/" + id);
   },
-  // Deletes the book with the given id
-  deleteFamily: function(id) {
-    return axios.delete("/api/families/" + id);
-  },
-  // Saves a book to the database
+  // Saves a family profile to the database
   saveFamily: function(familyData) {
     return axios.post("/api/families", familyData);
   }
